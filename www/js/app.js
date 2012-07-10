417b319d08e21c1be7da274cdf47c0ba1eeb538d
Chris Lee <cleer@cmu.edu>
2012-07-10 00:43:13 -0700

    Removed more junk

--- www/js/app.js
+++ www/js/app.js
@@ -349,71 +349,12 @@ var effects ={
   ]
 };
 
-var ui = {
-  step: 0,
-
-  colors: ['#900', '#06c', '#360'],
-
-  init: function() {
-    this.initCapture();
-  },
-
-  nextStep: function() {
-    if (this.step >= 2) return false;
-    this.step++;
-
-    var header = $('header');
-    header.css('background', this.colors[this.step]);
-
-    var stepEls = $('.step');
-    stepEls.removeClass('active');
-    var stepEl = $(stepEls[this.step]);
-    stepEl.addClass('active');
-
-    switch (this.step) {
-      case 1: this.initEffects(); break;
-      case 2: this.initShare(); break;
-    }
-
-    return true;
-  },
-
-  initCapture: function() {
-    var self = this;
-    $('#take').click(function() {
-      self.nextStep();
-    });
-    $('#effects').hide();
-    $('#share').hide();
-    $('#go-to-share').hide();
-  },
-
-  initEffects: function() {
-    $('#take').hide();
-    $('#effects').show();
-    $('#share').hide();
-    $('#go-to-share').show();
-    $('#go-to-share').click(function() {
-      ui.nextStep();
-    });
-  },
-
-  initShare: function() {
-    $('#take').hide();
-    $('#effects').hide();
-    $('#share').show();
-    $('#go-to-share').hide();
-  }
-}
-
-
-
 const WIDTH = 400;
 const HEIGHT = 400;
 
 // When you write javascript in separate files, list them as
 // dependencies along with jquery
-require(['jquery'], function($) {
+require(['jquery', 'ui'], function($, ui) {
   
   ui.init();
 
