e676019737a8a53511f4b237d165f85870072eed
Chris Lee <cleer@cmu.edu>
2012-07-10 01:04:18 -0700

    Refactor ui.js

--- www/js/ui.js
+++ www/js/ui.js
@@ -1,67 +1,63 @@
 define(['jquery', 'share'], function($, share) {
-  var ui = {
-    step: 0,
+  var step = 0;
+  var colors = ['#900', '#06c', '#360'];
 
-    colors: ['#900', '#06c', '#360'],
+  function nextStep() {
+    if (step >= 2) return false;
+    step++;
 
-    init: function() {
-      this.initCapture();
-    },
+    var header = $('header');
+    header.css('background', colors[step]);
 
-    nextStep: function() {
-      if (this.step >= 2) return false;
-      this.step++;
+    var stepEls = $('.step');
+    stepEls.removeClass('active');
+    var stepEl = $(stepEls[step]);
+    stepEl.addClass('active');
 
-      var header = $('header');
-      header.css('background', this.colors[this.step]);
-
-      var stepEls = $('.step');
-      stepEls.removeClass('active');
-      var stepEl = $(stepEls[this.step]);
-      stepEl.addClass('active');
-
-      switch (this.step) {
-        case 1: this.initEffects(); break;
-        case 2: this.initShare(); break;
-      }
-
-      return true;
-    },
-
-    initCapture: function() {
-      var self = this;
-      $('#take').click(function() {
-        self.nextStep();
-      });
-      $('#effects').hide();
-      $('#share').hide();
-      $('#go-to-share').hide();
-    },
-
-    initEffects: function() {
-      $('#take').hide();
-      $('#effects').show();
-      $('#share').hide();
-      $('#go-to-share').show();
-      $('#go-to-share').click(function() {
-        ui.nextStep();
-      });
-    },
-
-    initShare: function() {
-      $('#take').hide();
-      $('#effects').hide();
-      $('#share').show();
-      $('#go-to-share').hide();
-
-      $('#facebook').click(function () {
-        share.share('fb');
-      });
-      $('#twitter').click(function () {
-        share.share('t');
-      });
+    switch (step) {
+      case 1: initEffects(); break;
+      case 2: initShare(); break;
     }
-  }
 
-  return ui;
+    return true;
+  };
+
+  function initCapture() {
+    $('#take').click(function() {
+      nextStep();
+    });
+    $('#effects').hide();
+    $('#share').hide();
+    $('#go-to-share').hide();
+  };
+
+  function initEffects() {
+    $('#take').hide();
+    $('#effects').show();
+    $('#share').hide();
+    $('#go-to-share').show();
+    $('#go-to-share').click(function() {
+      nextStep();
+    });
+  };
+
+  function initShare() {
+    $('#take').hide();
+    $('#effects').hide();
+    $('#share').show();
+    $('#go-to-share').hide();
+
+    $('#facebook').click(function () {
+      share.share('fb');
+    });
+    $('#twitter').click(function () {
+      share.share('t');
+    });
+  };
+
+  return {
+    init: function() {
+      initCapture();
+    }
+  };
 });
