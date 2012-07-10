e9c80cda099c04692a8be1785962502303c488f7
Chris Lee <cleer@cmu.edu>
2012-07-10 01:08:52 -0700

    Use local jQuery always

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,5 @@
-
-// The code below uses require.js, a module system for javscript:
-// http://requirejs.org/docs/api.html#define
-
-// Set the path to jQuery, which will fall back to the local version
-// if google is down
 require.config({
-  paths: {'jquery':
-    ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-      'lib/jquery']}
+  paths: {'jquery': 'lib/jquery'}
 });
 
 // When you write javascript in separate files, list them as
